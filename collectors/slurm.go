e3d5c40c9d90a5e073fe6a446263f927024bd003
Tony Chen <runcong@gmail.com>
2024-01-25 11:43:09 -0500

    fix metrics duplication errors

--- collectors/slurm.go
+++ collectors/slurm.go
@@ -4,6 +4,7 @@ import (
 	"regexp"
 	"strconv"
 	"os/user"
+	"slices"
 
 	ps "github.com/mitchellh/go-ps"
 	cg "github.com/phpHavok/cgroups_exporter/cgroups"
@@ -62,6 +63,7 @@ func (collector *cgroupsSlurmCollector) Collect(ch chan<- prometheus.Metric) {
 		}
 	}
 	// Filter processes by children of slurmstepd processes
+	var matchesItem []string
 	for _, ssid := range slurmstepdIds {
 		for _, proc := range procs {
 			if proc.PPid() == ssid {
@@ -71,62 +73,65 @@ func (collector *cgroupsSlurmCollector) Collect(ch chan<- prometheus.Metric) {
 				}
 				slurmRegex := regexp.MustCompile(`/slurm(?:/uid_([^/]+))?(?:/job_([^/]+))?(?:/step_([^/]+))?(?:/task_([^/]+))?`)
 				matches := slurmRegex.FindStringSubmatch(string(cgroups.Cpuacct))
-				var (
-					user_id string
-					job_id  string
-					step_id string
-					task_id string
-				)
-				if len(matches) > 1 {
-					user_id = matches[1]
-				}
-				if len(matches) > 2 {
-					job_id = matches[2]
-				}
-				if len(matches) > 3 {
-					step_id = matches[3]
-				}
-				if len(matches) > 4 {
-					task_id = matches[4]
-				}
+				if slices.Contains(matchesItem, matches[0]) == false {
+					matchesItem = append(matchesItem, matches[0])
+					var (
+						user_id string
+						job_id  string
+						step_id string
+						task_id string
+					)
+					if len(matches) > 1 {
+						user_id = matches[1]
+					}
+					if len(matches) > 2 {
+						job_id = matches[2]
+					}
+					if len(matches) > 3 {
+						step_id = matches[3]
+					}
+					if len(matches) > 4 {
+						task_id = matches[4]
+					}
 
-				u, err := user.LookupId(user_id)
-				if err != nil {
-					log.Fatalf("unable to get user information for user id: %v", err)
-				}
+					u, err := user.LookupId(user_id)
+					if err != nil {
+						log.Fatalf("unable to get user information for user id: %v", err)
+					}
 
-				// cpuacctUsagePerCPUMetric
-				usagePerCPU, err := cgroups.Cpuacct.GetUsagePerCPU()
-				if err != nil {
-					log.Fatalf("unable to read cpuacct usage per cpu: %v", err)
-				}
-				for cpuID, cpuUsage := range usagePerCPU {
-					ch <- prometheus.MustNewConstMetric(collector.cpuacctUsagePerCPUMetric,
-						prometheus.GaugeValue, float64(cpuUsage), user_id, u.Username, job_id, step_id, task_id, strconv.Itoa(cpuID))
-				}
-				// memoryUsageInBytesMetric
-				memoryUsageBytes, err := cgroups.Memory.GetUsageInBytes()
-				if err != nil {
-					log.Fatalf("unable to read memory usage in bytes: %v", err)
-				}
-				ch <- prometheus.MustNewConstMetric(collector.memoryUsageInBytesMetric,
-					prometheus.GaugeValue, float64(memoryUsageBytes), user_id, u.Username, job_id, step_id, task_id)
-				// memoryLimitInBytesMetric
-				const slurm = true
-				memoryLimitBytes, err := cgroups.Memory.GetLimitInBytes(slurm)
-				if err != nil {
-					log.Fatalf("unable to read memory limit in bytes: %v", err)
-				}
-				ch <- prometheus.MustNewConstMetric(collector.memoryLimitInBytesMetric,
-					prometheus.GaugeValue, float64(memoryLimitBytes), user_id, u.Username, job_id, step_id, task_id)
-				// cpusetCPUsMetric
-				cpusetCPUs, err := cgroups.Cpuset.GetCpus()
-				if err != nil {
-					log.Fatalf("unable to read cpuset CPUs: %v", err)
-				}
-				for _, cpuID := range cpusetCPUs {
-					ch <- prometheus.MustNewConstMetric(collector.cpusetCPUsMetric,
-						prometheus.GaugeValue, float64(1), user_id, u.Username, job_id, step_id, task_id, strconv.Itoa(cpuID))
+					// cpuacctUsagePerCPUMetric
+					usagePerCPU, err := cgroups.Cpuacct.GetUsagePerCPU()
+					if err != nil {
+						log.Fatalf("unable to read cpuacct usage per cpu: %v", err)
+					}
+					for cpuID, cpuUsage := range usagePerCPU {
+						ch <- prometheus.MustNewConstMetric(collector.cpuacctUsagePerCPUMetric,
+							prometheus.GaugeValue, float64(cpuUsage), user_id, u.Username, job_id, step_id, task_id, strconv.Itoa(cpuID))
+					}
+					// memoryUsageInBytesMetric
+					memoryUsageBytes, err := cgroups.Memory.GetUsageInBytes()
+					if err != nil {
+						log.Fatalf("unable to read memory usage in bytes: %v", err)
+					}
+					ch <- prometheus.MustNewConstMetric(collector.memoryUsageInBytesMetric,
+						prometheus.GaugeValue, float64(memoryUsageBytes), user_id, u.Username, job_id, step_id, task_id)
+					// memoryLimitInBytesMetric
+					const slurm = true
+					memoryLimitBytes, err := cgroups.Memory.GetLimitInBytes(slurm)
+					if err != nil {
+						log.Fatalf("unable to read memory limit in bytes: %v", err)
+					}
+					ch <- prometheus.MustNewConstMetric(collector.memoryLimitInBytesMetric,
+						prometheus.GaugeValue, float64(memoryLimitBytes), user_id, u.Username, job_id, step_id, task_id)
+					// cpusetCPUsMetric
+					cpusetCPUs, err := cgroups.Cpuset.GetCpus()
+					if err != nil {
+						log.Fatalf("unable to read cpuset CPUs: %v", err)
+					}
+					for _, cpuID := range cpusetCPUs {
+						ch <- prometheus.MustNewConstMetric(collector.cpusetCPUsMetric,
+							prometheus.GaugeValue, float64(1), user_id, u.Username, job_id, step_id, task_id, strconv.Itoa(cpuID))
+					}
 				}
 			}
 		}
--- go.mod
+++ go.mod
@@ -1,9 +1,22 @@
 module github.com/phpHavok/cgroups_exporter
 
-go 1.16
+go 1.18
 
 require (
-	github.com/mitchellh/go-ps v1.0.0 // indirect
-	github.com/prometheus/client_golang v1.10.0 // indirect
+	github.com/mitchellh/go-ps v1.0.0
+	github.com/prometheus/client_golang v1.10.0
 	github.com/sirupsen/logrus v1.6.0
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.18.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
+	google.golang.org/protobuf v1.23.0 // indirect
 )
